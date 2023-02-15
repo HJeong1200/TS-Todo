ef4e80c7e446dff0da81ce7bd9be81a9967439e9
HJeong1200 <hjeong1200@gmail.com>
2023-02-16 00:49:05 +0900

    input component styling

--- ts-todo/src/App.tsx
+++ ts-todo/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import "./components/style.css";
 import React from "react";
+import InputField from "./components/InputField";
 
 function App() {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+
+      <InputField />
     </div>
   );
 }
--- ts-todo/src/components/InputField.tsx
+++ ts-todo/src/components/InputField.tsx
@@ -1,1 +1,10 @@
-export {};
+function InputField() {
+  return (
+    <form action="" className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input__submit">Go</button>
+    </form>
+  );
+}
+
+export default InputField;
