08605b2cb7733d07eacdc94ade5baadf92a70936
HJeong1200 <hjeong1200@gmail.com>
2023-03-05 00:47:54 +0900

    feat: add isDone, delete function

--- ts-todo/src/components/TodoCard.tsx
+++ ts-todo/src/components/TodoCard.tsx
@@ -5,15 +5,31 @@ interface CardProps extends Props {
 }
 
 const TodoCard = ({ todo, todos, setTodos }: CardProps) => {
-  const { content, isDone } = todo;
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <form className="todo__card">
-      <span className="todo__card--text">{content}</span>
+      <span className={`todo__card--text ${todo.isDone ? "done" : ""}`}>
+        {todo.content}
+      </span>
       <div className="todo__card--icons">
         <span className="icon">🖊</span>
-        <span className="icon">❌</span>
-        <span className="icon">✅</span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          ❌
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          ✅
+        </span>
       </div>
     </form>
   );
