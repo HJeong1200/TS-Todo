9f79233b9c40939569658991b2cd4160187309ff
HJeong1200 <hjeong1200@gmail.com>
2023-03-05 01:10:52 +0900

    feat: add edit function

--- ts-todo/src/components/TodoCard.tsx
+++ ts-todo/src/components/TodoCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { Props, Todo } from "./model";
 
 interface CardProps extends Props {
@@ -5,6 +6,14 @@ interface CardProps extends Props {
 }
 
 const TodoCard = ({ todo, todos, setTodos }: CardProps) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editContent, setEditContent] = useState<string>(todo.content);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, [edit]);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -17,13 +26,35 @@ const TodoCard = ({ todo, todos, setTodos }: CardProps) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, content: editContent } : todo
+      )
+    );
+
+    setEdit(!edit);
+  };
+
   return (
-    <form className="todo__card">
-      <span className={`todo__card--text ${todo.isDone ? "done" : ""}`}>
-        {todo.content}
-      </span>
+    <form className="todo__card" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editContent}
+          onChange={(e) => setEditContent(e.target.value)}
+        />
+      ) : (
+        <span className={`todo__card--text ${todo.isDone ? "done" : ""}`}>
+          {todo.content}
+        </span>
+      )}
       <div className="todo__card--icons">
-        <span className="icon">🖊</span>
+        <span className="icon" onClick={() => setEdit(!edit)}>
+          🖊
+        </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
           ❌
         </span>
